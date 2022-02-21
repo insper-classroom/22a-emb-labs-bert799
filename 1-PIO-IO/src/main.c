64942a56a88f890a45f1f98db1395cefaaa54433
bert799 <bernardocc@al.insper.edu.br>
2022-02-21 17:39:28 -0300

    Começando lab 1

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -23,10 +23,22 @@
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+#define LED_PIO   PIOC        
+#define LED_PIO_ID    ID_PIOC    // ID do periférico PIOC (controla LED)
+#define LED_PIO_IDX      8 
+#define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)
+
+// Configuracoes do botao
+#define BUT_PIO		PIOA
+#define BUT_PIO_ID  ID_PIOA
+#define BUT_PIO_IDX	11
+#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX) // esse já está pronto.
+
+// Configuracoes dos LEDs
+#define LED1_PIO	PIOA	
+#define LED1_PIO_ID ID_PIOA
+#define LED1_PIO_IDX 0
+#define LED1_PIO_IDX_MASK (1<< LED1_PIO_IDX)
 
 /************************************************************************/
 /* constants                                                            */
@@ -53,6 +65,27 @@ void init(void);
 // Função de inicialização do uC
 void init(void)
 {
+	// Initialize the board clock
+	sysclk_init();
+
+	// Desativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	// Ativa o PIO na qual o LED foi conectado
+	// para que possamos controlar o LED.
+	pmc_enable_periph_clk(LED_PIO_ID);
+	//Inicializa PC8 como saída
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	
+	//Config LEDs placa
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	//Inicializa PC8 como saída
+	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+	
+	// Inicializa PIO do botao
+	pmc_enable_periph_clk(BUT_PIO_ID);
+	// configura pino ligado ao botão como entrada com um pull-up.
+	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 0);
 
 }
 
@@ -69,7 +102,21 @@ int main(void)
   // aplicacoes embarcadas não devem sair do while(1).
   while (1)
   {
-
+	 pio_set(PIOA, LED1_PIO_IDX_MASK);      // Coloca 1 no pino LED
+	 delay_ms(1000);                        // Delay por software de 200 ms
+	 pio_clear(PIOA, LED1_PIO_IDX_MASK);    // Coloca 0 no pino do LED
+	 delay_ms(1000);                        // Delay por software de 200 ms
+	 if(!pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK)) {
+		for (int i = 0; i < 5; i++){
+			pio_set(PIOC, LED_PIO_IDX_MASK);      // Coloca 1 no pino LED
+			delay_ms(100);                        // Delay por software de 200 ms
+			pio_clear(PIOC, LED_PIO_IDX_MASK);    // Coloca 0 no pino do LED
+			delay_ms(100);                        // Delay por software de 200 ms
+		}
+	 }
+	 else {
+		pio_set(PIOC, LED_PIO_IDX_MASK);
+	 }
   }
   return 0;
 }
